4c919972556a1fcbfd3b90dac18d555cab1322fc
Crowdbotics <team@crowdbotics.com>
2021-08-20 12:41:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "steep_sea_29971Identifier",
+  url: "https://steep_sea_29971Identifier.botics.co",
+  api: "https://steep_sea_29971Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
